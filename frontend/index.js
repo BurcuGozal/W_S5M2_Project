7dd07a46cfd3e0a51433a710379ee2c69cc6dbd7
BurcuGozal <gozalburcu@gmail.com>
2024-08-23 14:51:35 -0600

    finished

--- frontend/index.js
+++ frontend/index.js
@@ -77,6 +77,7 @@ function moduleProject2() {
     let isDown = evt.key === keys.down
     let isLeft = evt.key === keys.left
     let isRight = evt.key === keys.right
+    let isSpacebar = evt.key === keys.space
 
     let targeted = document.querySelector('.targeted')
 
@@ -113,7 +114,34 @@ function moduleProject2() {
 
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
 
+    else if (isSpacebar){
+      let mosquito = targeted.firstChild
+      if(mosquito && mosquito.dataset.status === 'alive'){
+        mosquito.dataset.status = 'dead'
+        mosquito.parentElement.style.backgroundColor = 'red'
+      }
+     
+
+    }
+
     // 👉 TASK 5 - End the game 👈
+
+
+    let liveMosquitoes = document.querySelectorAll('[data-status=alive]')
+    if (!liveMosquitoes.length){
+      let elapsed = getTimeElapsed()
+      document.querySelector('p.info').textContent = 
+      `Extermination completed in ${elapsed / 1000} seconds!`
+
+      let restartBtn = document.createElement('button')
+      restartBtn.textContent = 'Restart'
+      restartBtn.addEventListener('click', ()  => {
+       location.reload() 
+      })
+      document.querySelector('h2').insertAdjacentElement('beforeend', restartBtn)
+      restartBtn.focus()
+    }
+  
   })
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
