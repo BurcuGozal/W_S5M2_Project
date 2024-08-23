d152b0b574d07b8988b5dc29a414280e3e9ddcb3
BurcuGozal <gozalburcu@gmail.com>
2024-08-23 13:24:50 -0600

    wip

--- frontend/index.js
+++ frontend/index.js
@@ -37,7 +37,12 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        if (!square.classList.contains('targeted')){
+          document.querySelector('.targeted').classList.remove('targeted')
+          square.classList.add('targeted')
+        }
       })
+
     }
   }
   document.querySelector('.row:nth-child(3)')
